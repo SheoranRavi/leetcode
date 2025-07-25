773686deb1a4672a6a4335384fbeb737a1b83741
Ravi Sheoran <ravisheoranr@gmail.com>
2025-07-25 12:15:49 +0530

    longest cycle in a graph success

--- src/Hard/longest-cycle-in-a-graph.go
+++ src/Hard/longest-cycle-in-a-graph.go
@@ -60,3 +60,47 @@ func dfsHelper(edges []int, visited []bool, parent []int, set map[int]bool, curr
 }
 
 // ----Simpler Solution----
+func longestCycleSimple(edges []int) int {
+	// since there'll be a single chain or cycle from each node
+	// we can track all the nodes in a map
+	// map: node -> steps (steps is the number of steps to reach there)
+	// if a node shows up again in the map, cycle is detected
+	// just check how many steps it took to get there again to get the cycle length
+
+	n := len(edges)
+	visited := make([]bool, n)
+	maxCycleLen := -1
+	for i := range n {
+		curr := i
+		if !visited[curr] && edges[curr] != -1 {
+			//another loop here to visit all nodes that can be visited from curr
+			next := edges[curr]
+			steps := make(map[int]int)
+			steps[curr] = 0
+			currSteps := 0
+			for !visited[next] {
+				//fmt.Println("visiting the node:", next)
+				_, ok := steps[next]
+				if ok {
+					// this means that this node has already come before
+					//fmt.Println("cycle detected at node:", next)
+					// no. of steps for curr node - the no. of steps for the cycle node(next) + 1 step to reach the next node
+					currLen := currSteps - steps[next] + 1
+					maxCycleLen = max(maxCycleLen, currLen)
+					break
+				}
+				steps[next] = steps[curr] + 1
+				currSteps = steps[next]
+				curr = next
+				next = edges[next]
+				if next == -1 {
+					break
+				}
+			}
+			for k, _ := range steps {
+				visited[k] = true
+			}
+		}
+	}
+	return maxCycleLen
+}
