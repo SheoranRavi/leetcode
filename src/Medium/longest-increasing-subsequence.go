86e68f84d63ee6ec3a4f9ba88829283daae7682c
Ravi Sheoran <ravisheoranr@gmail.com>
2025-07-28 18:50:27 +0530

    update

--- src/Medium/longest-increasing-subsequence.go
+++ src/Medium/longest-increasing-subsequence.go
@@ -26,24 +26,37 @@ func lengthOfLIS(nums []int) int {
 
 // O(n log n) solution
 // we keep track of the tail of LIS at each point
+// tail[i] would have the smallest number for tail ending at i+1
 
 func lengthOfLIS2(nums []int) int {
-	n := len(nums)
-	dp := make([]int, n)
-	for i := range n {
-		dp[i] = 1
-	}
-	for i := range n {
-		for j := i + 1; j < n; j++ {
-			if nums[j] > nums[i] {
-				dp[j] = max(dp[j], dp[i]+1)
-			}
+	tails := make([]int, 0)
+	tails = append(tails, nums[0])
+	for _, num := range nums {
+		// num either goes to end of tails
+		if num > tails[len(tails)-1] {
+			tails = append(tails, num)
+		} else {
+			idx := binarySearch(tails, num)
+			tails[idx] = num
 		}
 	}
+	return len(tails)
+}
 
-	maxLis := 1
-	for _, lis := range dp {
-		maxLis = max(lis, maxLis)
+func binarySearch(tails []int, target int) int {
+	// find the first number in tails that is >= target
+	l := 0
+	r := len(tails)
+	mid := (l + r) / 2
+	for l < r {
+		if tails[mid] > target {
+			r = mid
+		} else if tails[mid] < target {
+			l = mid + 1
+		} else {
+			return mid
+		}
+		mid = (l + r) / 2
 	}
-	return maxLis
+	return mid
 }
