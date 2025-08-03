e4317de79c2c8fe573db1da3d46c64ddf222b330
Ravi Sheoran <ravisheoranr@gmail.com>
2025-08-03 11:56:22 +0530

    largest rectangle histogram success

--- src/Hard/largest-rectangle-histogram.go
+++ src/Hard/largest-rectangle-histogram.go
@@ -29,3 +29,105 @@ func largestRectangleArea(heights []int) int {
 	}
 	return maxArea
 }
+
+// Using monotonic stacks, get the previous smaller and next smaller element for each height
+func largestRectangleArea(heights []int) int {
+
+	// at each index i, find the previous idx where element is smaller than heights[i]
+	//      and find the next idx where element is smaller than heights[i]
+	//      then multiply by width to get the area for this rectangle
+	// two stacks prevSmaller and nextSmaller -> use for getting the index
+	// two arrays prev, next -> store index of prev smaller and next smaller element
+	prevSmaller := NewStack()
+	nextSmaller := NewStack()
+	prev := make([]int, 0)
+	next := make([]int, 0)
+	for idx, v := range heights {
+		for {
+			top := prevSmaller.Peek()
+			if top == nil {
+				prev = append(prev, -1)
+				break
+			}
+			topIdx := top.(int)
+			if heights[topIdx] < v {
+				prev = append(prev, topIdx)
+				break
+			}
+			prevSmaller.Pop()
+		}
+		prevSmaller.Push(idx)
+	}
+
+	for idx := len(heights) - 1; idx >= 0; idx-- {
+		v := heights[idx]
+		for {
+			top := nextSmaller.Peek()
+			if top == nil {
+				next = append(next, -1)
+				break
+			}
+			topIdx := top.(int)
+			if heights[topIdx] < v {
+				next = append(next, topIdx)
+				break
+			}
+			nextSmaller.Pop()
+		}
+		nextSmaller.Push(idx)
+	}
+	slices.Reverse(next)
+
+	maxArea := 0
+	//fmt.Println(prev)
+	//fmt.Println(next)
+	for idx, h := range heights {
+		// find left and right ranges for this height
+		left := prev[idx]
+		right := next[idx]
+		if right == -1 {
+			right = len(heights)
+		}
+		width := right - left - 1
+		area := width * h
+		//fmt.Println("width for height", h, width)
+		maxArea = max(maxArea, area)
+	}
+	return maxArea
+
+}
+
+type Stack struct {
+	stack []int
+	n     int
+}
+
+func NewStack() Stack {
+	return Stack{
+		stack: make([]int, 0),
+		n:     0,
+	}
+}
+
+func (this *Stack) Pop() any {
+	if this.n == 0 {
+		return nil
+	}
+	old := this.stack
+	item := old[this.n-1:]
+	this.stack = old[:this.n-1]
+	this.n--
+	return item[0]
+}
+
+func (this *Stack) Peek() any {
+	if this.n == 0 {
+		return nil
+	}
+	return this.stack[this.n-1]
+}
+
+func (this *Stack) Push(item int) {
+	this.stack = append(this.stack, item)
+	this.n++
+}
