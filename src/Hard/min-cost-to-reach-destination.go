e99eb557993f1d8efa63903865d135a5b6543bf9
Ravi Sheoran <ravisheoranr@gmail.com>
2025-08-09 19:37:40 +0530

    min cost to reach destination in time TLE 2

--- src/Hard/min-cost-to-reach-destination.go
+++ src/Hard/min-cost-to-reach-destination.go
@@ -1,5 +1,10 @@
 package hard
 
+import (
+	"container/heap"
+	"math"
+)
+
 // tried by finding all paths to n-1, but this is obviously exponential so TLEs
 func minCost(maxTime int, edges [][]int, passingFees []int) int {
 	// find minTime to reach n-1
@@ -86,4 +91,86 @@ func dfs_min(node int, adj map[int][]Edge, visited []bool, passingFees []int, cu
 type Edge struct {
 	s, d int
 	time int
+}
+
+func minCost2(maxTime int, edges [][]int, passingFees []int) int {
+	// track the minCost for each (node, time step) state
+	n := len(passingFees)
+	minCost := make([][]int, n)
+	for i := range n {
+		minCost[i] = make([]int, maxTime+1)
+		for j := range maxTime + 1 {
+			minCost[i][j] = math.MaxInt32
+		}
+	}
+	minCost[0][0] = passingFees[0]
+	adj := buildAdj(edges)
+	item := Item{
+		node: 0,
+		time: 0,
+		cost: passingFees[0],
+	}
+	pq := &PQ{}
+	heap.Init(pq)
+	heap.Push(pq, &item)
+	for pq.Len() > 0 {
+		curr := heap.Pop(pq)
+		currItem := curr.(*Item)
+		// first time this node gets popped, it'll have the minimum cost.
+		if currItem.node == n-1 {
+			return currItem.cost
+		}
+		for _, edge := range adj[currItem.node] {
+			currTime := currItem.time + edge.time
+			currCost := currItem.cost + passingFees[edge.d]
+			if currTime <= maxTime && currCost < minCost[edge.d][currTime] {
+				heap.Push(pq, &Item{
+					node: edge.d,
+					time: currTime,
+					cost: currCost,
+				})
+			}
+		}
+	}
+	return -1
+}
+
+type Item struct {
+	node, time, cost int
+	index            int // index in the PQ
+}
+
+type PQ []*Item
+
+func (pq PQ) Len() int {
+	return len(pq)
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i].cost < pq[j].cost
+}
+
+func (pq *PQ) Push(x any) {
+	// place the item at the end
+	n := pq.Len()
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PQ) Pop() any {
+	// the heap package places the element at the end of the slice before calling this Pop function
+	n := pq.Len()
+	old := *pq
+	item := old[n-1]
+	item.index = -1
+	old[n-1] = nil
+	*pq = old[0 : n-1]
+	return item
 }
