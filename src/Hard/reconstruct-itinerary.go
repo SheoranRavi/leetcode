b4c5d746641280236b8db8630f2c847a9e53d959
Ravi Sheoran <ravisheoranr@gmail.com>
2026-02-26 18:41:20 +0530

    eulerian path

---
+++ src/Hard/reconstruct-itinerary.go
@@ -0,0 +1,45 @@
+package hard
+
+import "slices"
+
+// https://leetcode.com/problems/reconstruct-itinerary/
+func findItinerary(tickets [][]string) []string {
+	// eulerian path is what we need to find
+	// form the adjacency list
+	// then use the Hierholzer's algorithm
+	// need to store edges for a node in lexical order, we will skip this first
+
+	adj := make(map[string][]string)
+	for _, t := range tickets {
+		_, ok := adj[t[0]]
+		if !ok {
+			adj[t[0]] = make([]string, 0)
+		}
+		edges := adj[t[0]]
+		edges = append(edges, t[1])
+		// the underlying struct might change, so assign it back
+		adj[t[0]] = edges
+	}
+	// sort each edge list
+	for _, v := range adj {
+		slices.Sort(v)
+	}
+	// now run the algorithm
+	res := make([]string, 0)
+	dfs("JFK", adj, &res)
+	slices.Reverse(res)
+	return res
+}
+
+func dfs(s string, adj map[string][]string, res *[]string) {
+	for len(adj[s]) > 0 {
+		e := adj[s][0]
+		// remove e from adj
+		adj[s] = adj[s][1:]
+		// perform dfs
+		dfs(e, adj, res)
+	}
+	// add source to result
+	//fmt.Println("Adding to resArr:", s)
+	*res = append(*res, s)
+}
